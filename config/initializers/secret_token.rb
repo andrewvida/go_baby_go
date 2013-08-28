bfa2711d23d8036d33ae26310445a003e0af17cf
Andrew Vida <andy@testdouble.com>
2013-08-27 23:10:58 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoBabyGo::Application.config.secret_key_base = '0a410cb8de871cd9c894161d5e0d0969769f26c715cb1b98438f5b2c6ad3ee074a2bf5e79bbc90e22949fd39ebc35e853b08c67d5df1e82f652b5066f1349b67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoBabyGo::Application.config.session_store :cookie_store, key: '_go_baby_go_session'
