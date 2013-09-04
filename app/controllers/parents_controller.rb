c3ce49fc75cda56a0188e5539f12241ed119a877
Andrew Vida <andy@testdouble.com>
2013-09-03 22:36:26 -0400

    added user signup

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,4 +1,26 @@
 class ParentsController < ApplicationController
   def new
+    @parent = Parent.new
   end
+
+  def show
+    @parent = Parent.find(params[:id])
+  end
+
+  def create
+    @parent = Parent.new(parent_params)
+    if @parent.save
+      flash[:success] = "Welcome to the Our App!"
+      redirect_to @parent
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def parent_params
+      params.require(:parent).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 GoBabyGo::Application.routes.draw do
-  get "parents/new"
   get "home/index"
+  match '/signup', to: 'parents#new', via: 'get'
+  resources :parents
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
